74c0646595414382ac021d131bd5e10945fa3a40
lavander8 <lavander8@rambler.su>
2025-10-02 21:41:38 +0300

    Исправлены функции, удалено лишнее

--- keywords.py
+++ keywords.py
@@ -1,35 +1,12 @@
 def max_number(a, b):
-    if a < 0 or b < 0:
-        return "Ошибка: отрицательное число не может быть больше."
-    elif a == b:
-        return f"Ошибка: из чисел ({a}, {b}) невозможно выбрать наибольшее."
-    elif a > b:
+    if a > b:
         return a
     else:
         return b
 
 
-def test_max_number():
-    assert max_number(1, -1) == "Ошибка: отрицательное число не может быть больше."
-    assert max_number(1, 1) == "Ошибка: из чисел (1, 1) невозможно выбрать наибольшее."
-    assert max_number(4, 8) == 8
-    assert max_number(6, 2) == 6
-
-
-test_max_number()
-print("Все тесты пройдены!")
-
-print(max_number(1, -1))
-print(max_number(1, 1))
-print(max_number(4, 8))
-print(max_number(6, 2))
-
-
 def empty_function():
     pass
-
-
-print('Функция определена, но пока ничего не делает.')
 
 
 def even_numbers(n):
@@ -38,5 +15,11 @@ def even_numbers(n):
             yield i
 
 
-print(list(even_numbers(8)))
+def test_max_number():
+    assert max_number(1, -1) == 1, "Ошибка: из чисел (1, -1) наибольшее будет 1."
+    assert max_number(1, 1) == 1, "Ошибка: из чисел (1, 1) наибольшее должно быть 1."
+    assert max_number(4, 8) == 8, "Ошибка: из чисел (4, 8) наибольшее будет 8."
+    assert max_number(6, 2) == 6, "Ошибка: из чисел (4, 6) наибольшее будет 6."
 
+
+test_max_number()
